c5f27216273f086178558145f79914ab88e81941
kier-mirko <mirkotolentino1@gmail.com>
2025-06-06 20:49:54 +0200

    make linux build work again

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p build
-CFLAGS="-g3 -I../src/third_party/glfw/include -I../src/third_party/glad/include -L../src/third_party/glfw/lib"
+CFLAGS="-g3 -I../src/third_party/glfw/include -I../src/third_party/glad/include -I../src/third_party -L../src/third_party/glfw/lib"
 LDLIBS="-lglfw3 -lGL -lm"
 cd build >/dev/null
 cc $CFLAGS $(realpath ../src/cg/main.c) $LDLIBS
